1da50a2c4ca63d7bfab8e064e3fcb825f7224504
alex <info@smartscripts.nl>
2026-02-20 15:59:25 +0100

    [hotfix] Crash in BookInfoManager

--- koreader-settings-folder/extensions/bookinfomanager.lua
+++ koreader-settings-folder/extensions/bookinfomanager.lua
@@ -679,7 +679,7 @@ function BookInfoManager:deleteBookInfo(full_path)
     local stmt = self.db_conn:prepare(query)
     stmt:bind(directory, filename)
     stmt:step() --* commited
-    stmt = KOR.databases:closeStmts(stmt)
+    stmt = KOR.databases:closeInfoStmts(stmt)
     self:closeDbConnection()
 end
 
