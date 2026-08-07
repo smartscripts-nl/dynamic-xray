2e92ceb127ba0198f50bd98d41a056ac6ad85518
alex <info@smartscripts.nl>
2026-08-07 12:11:42 +0200

    [hotfix] Don't try to load extension Debug

--- koreader-settings-folder/plugins/xraycontroller.koplugin/extensions/kor.lua
+++ koreader-settings-folder/plugins/xraycontroller.koplugin/extensions/kor.lua
@@ -16,7 +16,6 @@ local pairs = pairs
 --- @field colors Colors
 --- @field columntexts ColumnTexts
 --- @field databases Databases
---- @field debug Debug
 --- @field dialogs Dialogs
 --- @field dialogsqueue DialogsQueue
 --- @field document CreDocument
@@ -80,7 +79,6 @@ local KOR = WidgetContainer:new{
 	closingswipes = nil,
 	colors = nil,
 	columntexts = nil,
-	debug = nil,
 	dialogs = nil,
 	dialogsqueue = nil,
 	ebookmetadata = nil,
@@ -119,7 +117,6 @@ local KOR = WidgetContainer:new{
 		--! this first block contains extensions which are needed by other extensions and therefor must be initialized first:
 		"dialogs",
 		"dialogsqueue",
-		"debug",
 		"files",
 		"tables",
 		"keyevents",
