e2fdc2c919c760609ae56d1b52fe411eb9423ccb
alex <info@smartscripts.nl>
2026-09-12 13:57:16 +0200

    Add gesture for showing form to add new item
    Entry "Show form for new Xray item" under section "Reflowable documents"

--- koreader-settings-folder/plugins/xraycontroller.koplugin/xraycontroller/xraycontroller.lua
+++ koreader-settings-folder/plugins/xraycontroller.koplugin/xraycontroller/xraycontroller.lua
@@ -159,6 +159,7 @@ end
 
 --- @private
 function XrayController:onDispatcherRegisterActions()
+    Dispatcher:registerAction("add_xray_item", { category = "none", event = "ShowNewItemForm", title = "Show form for new Xray item", rolling = true })
     Dispatcher:registerAction("show_items", { category = "none", event = "ShowList", title = DX.d:getControllerEntryName("Show xray-items in this book/series"), rolling = true })
     Dispatcher:registerAction("show_xray_page_navigator", { category = "none", event = "ShowPageNavigator", title = DX.d:getControllerEntryName("Show Xray Page Navigator"), rolling = true })
     Dispatcher:registerAction("add_xray_item", { category = "none", event = "AddNewXrayItem", title = DX.d:getControllerEntryName("Add an Xray item"), rolling = true })
